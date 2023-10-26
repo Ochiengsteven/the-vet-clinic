0a775db1ad798b22f31ac23a902d80a3eda32570
Ochiengsteven <stevenochieng6305@gmail.com>
2023-10-26 12:03:56 +0300

    ADD SCHEMA AND DATA SQL

--- data.sql
+++ data.sql
@@ -24,3 +24,46 @@ INSERT INTO animals VALUES (10, 'Blossom', '1998-10-13', 3, TRUE, 17);
 
 -- Insert Ditto's information
 INSERT INTO animals VALUES (11, 'Ditto', '2022-05-14', 4, TRUE, 22);
+
+-- insert values to the owners table
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+-- Insert data into the species table
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+-- Update the species_id for animals whose names end in "mon" to be "Digimon"
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Digimon')
+WHERE name LIKE '%mon';
+
+-- Update the species_id for all other animals to be "Pokemon"
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+WHERE name NOT LIKE '%mon';
+
+-- Update owner_id for animals based on owner information
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Jennifer Orwell')
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Bob')
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Melody Pond')
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Dean Winchester')
+WHERE name IN ('Angemon', 'Boarmon');
--- schema.sql
+++ schema.sql
@@ -13,3 +13,34 @@ CREATE TABLE animals(
 -- add column species
 ALTER TABLE animals
 ADD species VARCHAR(50);
+
+-- create table owners
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    age INT
+);
+
+-- create table species
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- set animals id to primary key and auto increment
+ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE animals ADD COLUMN id SERIAL PRIMARY KEY;
+
+-- Remove the "species" column and Add the "species_id" column as a foreign key referencing the "species" table
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species (id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners (id);
